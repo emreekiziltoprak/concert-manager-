e8d4ff77ce22cdb1add848f360556e2d7375e4ea
emreekiziltoprak <emre.kzltprkk@gmail.com>
2026-08-14 17:31:04 +0300

    validation logic is updated with zod, and validateResponse middleware function is introduced

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,8 +1,9 @@
 import type { Request, Response } from "express";
 import * as authService from "../services/authService";
 import { errorMessage } from "../utils/errorMessage";
+import { LoginInput, RegisterInput } from "../schemas/auth.schema";
 
-const register = async (req: Request, res: Response) => {
+const register = async (req: Request<{}, {}, RegisterInput>, res: Response) => {
 
     try {
         const user = await authService.register(req.body);
@@ -13,7 +14,7 @@ const register = async (req: Request, res: Response) => {
     }
 }
 
-const login = async (req: Request, res: Response) => {
+const login = async (req: Request<{},{}, LoginInput>, res: Response) => {
     try{
         const successResult = await authService.login(req.body);
         res.status(200).json(successResult);
