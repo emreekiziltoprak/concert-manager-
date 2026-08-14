303bded7552dcbe931b013fad1b24d53d4724365
emreekiziltoprak <emre.kzltprkk@gmail.com>
2026-08-14 22:41:05 +0300

    revert stacked changes

--- backend/dist/src/services/stockCalculation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculateAvailableStock = void 0;
-const calculateAvailableStock = (totalCount, reservedQuantity) => {
-    return totalCount - reservedQuantity;
-};
-exports.calculateAvailableStock = calculateAvailableStock;
-//# sourceMappingURL=stockCalculation.js.map
